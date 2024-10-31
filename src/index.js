42edd6425411da9861ec114719178627cfc89098
elfaroliver <eos35@hi.is>
2024-10-31 19:53:04 +0000

    Tók sýnilausn úr html, hehe

--- src/index.js
+++ src/index.js
@@ -188,7 +188,7 @@ function render(container, locations, onSearch, onSearchMyLocation) {
   // Búum til <header> með beinum DOM aðgerðum
   const headerElement = document.createElement('header');
   const heading = document.createElement('h1');
-  heading.appendChild(document.createTextNode('<fyrirsögn>'));
+  heading.appendChild(document.createTextNode('Veðrið! Hér! Þar! En ekki alls staðar'));
   headerElement.appendChild(heading);
   parentElement.appendChild(headerElement);
 
